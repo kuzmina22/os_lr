6c26ce8047a26d70dcb7cb0dacbd2d6431b53af0
kuzmina22 <kyana2016@mail.ru>
2016-05-16 22:01:07 +0300

    Update lr2.c

--- lr2.c
+++ lr2.c
@@ -1,3 +1,6 @@
+/* программа, реализующая базовый функционал оболочки + 
+перенаправление ввода/вывода с помощью системных вызовов open и dup2
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
@@ -5,50 +8,66 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-int main(){
- int i, j, c, in, STDOUT_Forward, STDIN_Forward; 
- char argvp[16][80];
- char* argv[16];
- printf("$"); 
- i=0; j=0; in=0; 
- STDOUT_Forward=0; STDIN_Forward=0; 
- while ((c=getchar())!=EOF) {
-   if (c==' ' || c=='\n' || c=='>' || c=='<') {
-     if (in==1) {in=0; argvp[i][j]='\0'; argv[i]=&argvp[i]; ++i; j=0;}          
-   } else {argvp[i][j]=c; ++j; in=1;};        
-  
-   if (c=='>') {STDOUT_Forward=i;};
-   if (c=='<') {STDIN_Forward=i;};
+#define MAX_WORDS 16
+#define MAX_WORD_LEN 80
 
-   if (c=='\n') {
-     argv[i]=NULL;
+int main() {
+ char argvp[MAX_WORDS][MAX_WORD_LEN];
+ int i = 0; 
+ int j = 0; 
+ int in = 0; 
+ int STDOUT_Forward = 0; 
+ int STDIN_Forward = 0; 
+ printf("$");
+ int c;
+ while ((c = getchar())!=EOF) {
+   if (c == ' ' || c == '\n' || c == '>' || c == '<') {
+     if (in == 1) {
+       in = 0; 
+       argvp[i][j] = '\0'; 
+       ++i; 
+       j = 0;
+     }          
+   } else {
+      argvp[i][j] = c; 
+      ++j; 
+      in = 1;
+     }        
+   if (c == '>') {
+     STDOUT_Forward = i;
+   }
+   if ( c== '<') {
+     STDIN_Forward = i;
+   }
+   if ( c== '\n') {
+     argvp[i] = NULL;
      pid_t pid = fork();
      if (!pid) { // child branch
-       if (STDOUT_Forward!=0) { 
-          int fd_out = open(argv[STDOUT_Forward], O_WRONLY | O_CREAT | O_TRUNC, 0666);
-          if (fd_out == -1) {
-                 perror("open");
-                 return EXIT_FAILURE;
-           }
-          if (-1 == dup2(fd_out, STDOUT_FILENO)) {
-                 perror("dup2");
-                 return EXIT_FAILURE;
-          }
-          argv[STDOUT_Forward]=NULL;
+       if (STDOUT_Forward != 0) { 
+         int fd_out = open(&argvp[STDOUT_Forward], O_WRONLY | O_CREAT | O_TRUNC, 0666);
+         if (fd_out == -1) {
+           perror("open");
+           return EXIT_FAILURE;
+         }
+         if (-1 == dup2(fd_out, STDOUT_FILENO)) {
+           perror("dup2");
+           return EXIT_FAILURE;
+         }
+         argvp[STDOUT_Forward]=NULL;
        }
-       if (STDIN_Forward!=0) { 
-         int fd_in = open(argv[STDIN_Forward], O_RDONLY);
+       if (STDIN_Forward != 0) { 
+         int fd_in = open(&argvp[STDIN_Forward], O_RDONLY);
          if (fd_in == -1) {
-            perror("open");
-            return EXIT_FAILURE;
+           perror("open");
+           return EXIT_FAILURE;
          }
          if (-1 == dup2(fd_in, STDIN_FILENO)) {
            perror("dup2");
            return EXIT_FAILURE;
          }
-         argv[STDIN_Forward]=NULL;
+         argvp[STDIN_Forward]=NULL;
        }
-       int rv = execvp(argv[0], argv);
+       int rv = execvp(&argvp[0], &argvp);
        if (rv == -1) {
           perror("execvp");
           return EXIT_FAILURE;
@@ -60,11 +79,13 @@ int main(){
        perror("wait");
        return EXIT_FAILURE;
      }
-     i=0; j=0; in=0; 
-     STDOUT_Forward=0; STDIN_Forward=0;
+     i = 0; 
+     j = 0; 
+     in = 0; 
+     STDOUT_Forward = 0; 
+     STDIN_Forward = 0;
      printf("$");
    }
  }
- printf("\n");
  return EXIT_SUCCESS;
 }
